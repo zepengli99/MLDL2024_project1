4e915391ceeef680704c9f1cd4a1704e358ada8a
ffdepp <2606616925@qq.com>
2024-04-30 21:33:54 +0200

    update cityscapes.py

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,21 +1,49 @@
+import os
+from PIL import Image
+import numpy as np
+import torch
+from PIL import Image
 from torch.utils.data import Dataset
-
 # TODO: implement here your custom dataset class for Cityscapes
 
 
-class CityScapes(Dataset):
-    def __init__(self):
-        super(CityScapes, self).__init__()
-        # TODO
+class CityscapesDataset(Dataset):
+    def __init__(self, root_dir, split='train', transform=None):
+        self.root_dir = root_dir
+        self.split = split
+        self.transform = transform
+        self.image_dir = os.path.join(self.root_dir, 'images', self.split)
+        self.label_dir = os.path.join(self.root_dir, 'gtFine', self.split)
+        self.image_folders = os.listdir(self.image_dir)
+        self.images = []
 
-        pass
-
-    def __getitem__(self, idx):
-        # TODO
-
-        pass
-
+        for folder in self.image_folders:
+            images_in_folder = os.listdir(os.path.join(self.image_dir, folder))
+            self.images.extend(images_in_folder)
+        
     def __len__(self):
-        # TODO
+        return len(self.images)
+    
+    def __getitem__(self, idx): 
+        folder_name = self.images[idx].split('_')[0]
+        img_name = os.path.join(self.image_dir, folder_name, self.images[idx])
+        label_name = os.path.join(self.label_dir, folder_name, self.images[idx].replace('leftImg8bit', 'gtFine_labelTrainIds'))
+        
+        image = Image.open(img_name).convert('RGB')
+        label = torch.tensor(np.array(Image.open(label_name)))
+        
+        # This code need to updated.
+        # if self.transform:
+        #     image = self.transform(image)
+        
+        return image, label
 
-        pass
+# An example about how to use this class to load Cityspaces dataset
+# import torchvision.transforms as transforms
+# transform = transforms.Compose([
+#     transforms.Resize((1024,512)) # resize to 1024*512  # need to be updated
+#     transforms.ToTensor()
+# ])
+
+# You should specify the path of your Cityspaces dataset
+# train_dataset = CityscapesDataset('datasets\\Cityspaces', transform=transform) 
