7d0e17939a0f377e67945d6a0fa7aa17e1f5b734
ffdepp <2606616925@qq.com>
2024-05-14 17:24:48 +0200

    1.Add data augmentation 2.Add classes and classes_ids method

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -4,10 +4,11 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 from gta5Labels import GTA5Labels_TaskCV2017
+from torchvision.transforms import functional as F
+from torchvision import transforms
 
 
 class GTA5(Dataset):
-    label_map = GTA5Labels_TaskCV2017().trans_label_map()
 
     class PathPair_ImgAndLabel:
         IMG_DIR_NAME = "images"
@@ -36,10 +37,15 @@ class GTA5(Dataset):
             tid_paths = os.listdir(tid_dir)
             return tid_paths
 
-    def __init__(self, root, transform=None):
+    def __init__(self, root, transform=None, target_transform=None):
         self.root = root
+        self.label_map = GTA5Labels_TaskCV2017()
         self.transform = transform
+        self.target_transform = target_transform
         self.paths = self.PathPair_ImgAndLabel(root=self.root)
+
+    def classes(self):
+        return self.label_map.support_id_list
 
     def __len__(self):
         return len(self.paths)
@@ -51,13 +57,56 @@ class GTA5(Dataset):
         if isPath:
             return img_path, tid_path
 
-        img = np.array(Image.open(str(img_path)).convert('RGB'))
-        img = torch.tensor(img)
-
-        tid = np.array(Image.open(str(tid_path)))
-        tid = torch.tensor(tid)
+        img = Image.open(str(img_path)).convert('RGB')
+        tid = Image.open(str(tid_path))
 
         if self.transform:
             img = self.transform(img)
-            
-        return img, tid
+        
+        if self.target_transform:
+            tid = self.target_transform(tid)
+
+        return img, torch.tensor(np.array(tid)).long()
+       
+
+# An example about how to use this class to load GTA5 dataset
+# transform = transforms.Compose([
+#     transforms.Resize((720,1280), interpolation=transforms.InterpolationMode.NEAREST),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# ])
+
+# target_transform = transforms.Compose([
+#     transforms.Resize((720,1280), interpolation=transforms.InterpolationMode.NEAREST)
+# ])
+
+# transform with augmentation
+# transform_aug = transforms.Compose([    
+#     # flip/rotarion/crop
+#     transforms.RandomHorizontalFlip(p=0.5),
+#     transforms.RandomVerticalFlip(p=0.5),
+#     transforms.RandomRotation(degrees=30),
+#     transforms.RandomResizedCrop(size=(720,1280), interpolation=transforms.InterpolationMode.NEAREST),
+
+#     # transformation for colorful image
+#     transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1),
+
+#     # gaussian blur
+#     transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),
+      
+#     *transform.transforms
+
+# ])
+
+# transform with augmentation
+# target_transform_aug = transforms.Compose([
+#     # flip/rotarion/crop
+#     transforms.RandomHorizontalFlip(p=0.5),
+#     transforms.RandomVerticalFlip(p=0.5),
+#     transforms.RandomRotation(degrees=30),
+#     transforms.RandomResizedCrop(size=(720,1280), interpolation=transforms.InterpolationMode.NEAREST),
+
+#     *target_transform.transforms
+# ])
+
+# train_dataset = GTA5('datasets\\GTA5', transform=transform_aug, target_transform=target_transform_aug)
